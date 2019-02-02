6f3121802f67ec6561f742e8e1fee380ae08b32d
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-02-02 14:31:51 -0200

    sdm845-common: dirac: Only call setEnabled on boot completed
    Signed-off-by: saikiran <bjsaikiran@gmail.com>
    Signed-off-by: Saikiran <bjsaikiran@gmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -34,6 +34,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     public void onReceive(final Context context, Intent intent) {
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
         ThermalUtils.startService(context);
-        new DiracUtils(context);
+        new DiracUtils(context).onBootCompleted();
     }
 }
