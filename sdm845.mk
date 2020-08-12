142720aa2ed9db0358aa095f29dd4a11996941a3
Lucchetto <lucchetto.tie@live.com>
2020-08-12 14:14:36 +0200

    sdm845-common: create dummy libqti-perfd-client
    * proprietary perfd blobs can finally be nuked without breaking goodix
    * we could even map the functions to use libperfmgr powerhints in the future
    
    Change-Id: I56706fb47cc791a58e6706d9c0791f2e9d168f15
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Signed-off-by: Saikiran <bjsaikiran@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -262,6 +262,10 @@ PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-vendorcompat \
     libprotobuf-cpp-lite-vendorcompat
 
+# Perfd (dummy)
+PRODUCT_PACKAGES += \
+    libqti-perfd-client
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.3-service.xiaomi-libperfmgr \
